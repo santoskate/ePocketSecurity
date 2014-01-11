c07ea7eb32131e272b91289d6c4c7cf2d5d93429
kate <santos.kate@gmail.com>
2014-01-11 22:40:53 +0000

    teste ola

--- master/DroidCamSecurity/src/businessLayer/Authentication.java
+++ master/DroidCamSecurity/src/businessLayer/Authentication.java
@@ -5,7 +5,7 @@ public class Authentication {
 	public static void registerAccount(String email, String password, 
 			String confPwd, String ipCam, String camPwd) {
 		
-		
+		System.out.println("ola");
 	}
 
 }
